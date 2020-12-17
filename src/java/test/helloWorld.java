6ee110dcd10dcdb75f8511ef9e3277907a712f9b
Leonardo <lcsi3637@gmail.com>
2020-12-17 00:34:28 -0500

    there were a problem of curly braces

--- src/java/test/helloWorld.java
+++ src/java/test/helloWorld.java
@@ -6,14 +6,15 @@ public class helloWorld {
 
     }
 
-   public static void method1(){
-       System.out.println("method1");
-       
-  
-   }
-    public static void method2(){
-       System.out.println("method2");
-       
-  
+    public static void method1() {
+        System.out.println("method1");
 
+
+    }
+
+    public static void method2() {
+        System.out.println("method2");
+
+
+    }
 }
