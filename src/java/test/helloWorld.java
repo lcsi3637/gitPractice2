fad9ab164205b035ffb422544365f3ccbd9db4c1
Leonardo <lcsi3637@gmail.com>
2020-12-16 19:09:40 -0500

    second commit

--- src/java/test/helloWorld.java
+++ src/java/test/helloWorld.java
@@ -1,2 +1,7 @@
-package test;public class helloWorld {
+package test;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("this is my first code line");
+    }
 }
