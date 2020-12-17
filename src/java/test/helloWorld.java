fbcd715ac34d6ee0288b87358e23358ed5b9caa4
Leonardo <lcsi3637@gmail.com>
2020-12-16 19:14:53 -0500

    fifth commit

--- src/java/test/helloWorld.java
+++ src/java/test/helloWorld.java
@@ -3,12 +3,7 @@ package test;
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("this is my first code line");
-            
-            
-            
-            
-            
-            //gozumsun
+
     }
 
    public static void method1(){
