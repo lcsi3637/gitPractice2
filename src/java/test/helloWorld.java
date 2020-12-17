f26f943ef739f696d151494ca3ba033c3ba33a9a
Leonardo <lcsi3637@gmail.com>
2020-12-16 23:12:35 -0500

    comment silindi

--- src/java/test/helloWorld.java
+++ src/java/test/helloWorld.java
@@ -5,7 +5,7 @@ public class helloWorld {
         System.out.println("this is my first code line");
 
     }
-//tekrar bir dahi ask ile
+
    public static void method1(){
        System.out.println("method1");
        
