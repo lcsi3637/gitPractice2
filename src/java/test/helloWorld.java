87d53d9476ccbbada6db45e5a067b58f23e2ec7b
saban inci <64227008+sabaninci@users.noreply.github.com>
2020-12-17 00:27:24 -0500

    Update helloWorld.java
    added one more method which called method2

--- src/java/test/helloWorld.java
+++ src/java/test/helloWorld.java
@@ -7,6 +7,9 @@ public class helloWorld {
     }
 
    public static void method1(){
+       System.out.println("method1"); 
+       
+       public static void method2(){
        System.out.println("method1");
        
   
